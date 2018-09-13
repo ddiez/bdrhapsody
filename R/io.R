c60f8ebc5c00cc622618d6bca005d9b190c72db5
Diego Diez <diego10ruiz@gmail.com>
2018-09-13 16:04:34 +0900

    rowData() should be set in read_bd.character().

--- R/io.R
+++ R/io.R
@@ -19,22 +19,11 @@ read_bd.data.frame <- function(x, split = FALSE) {
     for (k in seq_len(nrow(x))) {
       ytmp <- read_bd(x[["filename"]][k], samplename = x[["samplename"]][k])
 
+      # add additional colData information.
       cdata <- colData(ytmp) %>% as.data.frame()
       cdata <- left_join(cdata, x, by = c("samplename", "filename"))
       rownames(cdata) <- cdata[["cell_index"]]
       colData(ytmp) <- DataFrame(cdata)
-
-      # set rowData.
-      ann <- strsplit(rownames(ytmp), "\\|")
-      ann <- tibble(
-        id = rownames(ytmp),
-        symbol = sapply(ann, `[`, 1),
-        accession = sapply(ann, `[`, 2),
-        note = sapply(ann, `[`, 3)
-      )
-
-      rdata <- DataFrame(ann, row.names = rownames(ytmp))
-      rowData(ytmp) <- rdata
 
       y[[k]] <- ytmp
     }
@@ -42,23 +31,11 @@ read_bd.data.frame <- function(x, split = FALSE) {
   } else {
     y <- read_bd(x[["filename"]], x[["samplename"]])
 
-    # set colData.
+    # add additional colData information.
     cdata <- colData(y) %>% as.data.frame()
     cdata <- left_join(cdata, x, by = c("samplename", "filename"))
     rownames(cdata) <- cdata[["cell_index"]]
     colData(y) <- DataFrame(cdata)
-
-    # set rowData.
-    ann <- strsplit(rownames(y), "\\|")
-    ann <- tibble(
-      id = rownames(y),
-      symbol = sapply(ann, `[`, 1),
-      accession = sapply(ann, `[`, 2),
-      note = sapply(ann, `[`, 3)
-    )
-
-    rdata <- DataFrame(ann, row.names = rownames(y))
-    rowData(y) <- rdata
   }
 
   y
@@ -68,8 +45,6 @@ read_bd.data.frame <- function(x, split = FALSE) {
 #'
 #' @export
 read_bd.character <- function(filename, samplename = NULL) {
-  require(SingleCellExperiment)
-
   if (is.null(samplename))
     samplename <- paste0("S", sprintf("%05d", length(filename)))
 
@@ -78,13 +53,25 @@ read_bd.character <- function(filename, samplename = NULL) {
 
   y <- to_matrix(x %>% select(-samplename, -filename), "cell_index") %>% t()
 
+  # colData.
   cdata <- x %>% select(samplename, filename, cell_index)
+
+  # set rowData.
+  ann <- strsplit(rownames(y), "\\|")
+  rdata <- tibble(
+    id = rownames(y),
+    symbol = sapply(ann, `[`, 1),
+    accession = sapply(ann, `[`, 2),
+    note = sapply(ann, `[`, 3),
+    row.names = rownames(y)
+  )
 
   SingleCellExperiment::SingleCellExperiment(
     assays = list(
       counts = y,
       logcounts = log10(y + 1)),
-    colData = cdata
+    colData = cdata,
+    rowData = rdata
   )
 }
 
@@ -95,7 +82,7 @@ read_bd_as_df <- function(filename, samplename = NULL) {
     progress = FALSE,
     col_types = cols(.default = col_integer())
   ) %>%
-    dplyr::rename(cell_index = Cell_Index)
+    rename(cell_index = Cell_Index)
 
   if (!is.null(samplename))
     x <- x %>% mutate(samplename = !!samplename, cell_index = paste0(!!samplename, "_", cell_index))
