e9a95d59c1305993de2393796df4264702a6364c
Diego Diez <diego10ruiz@gmail.com>
2018-10-16 16:46:31 +0900

    Fix dependency.

--- R/bdrhapsody-package.R
+++ R/bdrhapsody-package.R
@@ -5,7 +5,7 @@
 #'
 #' @import SummarizedExperiment SingleCellExperiment tibble
 #' @importFrom S4Vectors DataFrame
-#' @importFrom dplyr %>% bind_rows select mutate rename left_join
+#' @importFrom dplyr %>% bind_rows select mutate rename left_join pull
 #' @importFrom readr read_csv cols col_integer
 #' @importFrom AnnotationDbi mapIds
 NULL
